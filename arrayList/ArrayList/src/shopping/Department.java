3ee624948fd63fcc1617d094a4df7c8b3d01d356
vedha1997 <vedha.venkataraman@atmecs.com>
2019-06-25 07:43:57 -0600

    push

---
+++ arrayList/ArrayList/src/shopping/Department.java
@@ -0,0 +1,15 @@
+package shopping;
+
+public class Department {
+	int did;
+	String dname;
+	String designation;
+
+	public Department(int did, String dname, String designation) {
+
+		this.did = did;
+		this.dname = dname;
+		this.designation = designation;
+	} 
+
+}
